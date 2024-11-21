ba8d60fbdc1a3253c5a787a674e3bcd4984a5475
GuiLSDias <glsd273@gmail.com>
2024-11-21 20:24:54 -0300

    commit

--- script.js
+++ script.js
@@ -65,5 +65,25 @@ function initGoogleSheetsApi() {
     });
 }
 
+let indice = 0;
+
+function mudarImagem(direcao) {
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+  
+  // Atualiza o índice com base na direção do clique
+  indice += direcao;
+  
+  // Verifica se o índice ultrapassou o limite
+  if (indice < 0) {
+    indice = totalSlides - 1;
+  } else if (indice >= totalSlides) {
+    indice = 0;
+  }
+  
+  // Atualiza a posição das imagens
+  const imagensContainer = document.querySelector('.imagens');
+  imagensContainer.style.transform = `translateX(-${indice * 100}%)`;
+}
 // Carrega a API do Google Sheets e inicia a aplicação
 gapi.load('client', initGoogleSheetsApi);
